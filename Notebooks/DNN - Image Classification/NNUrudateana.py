bc561c94b27da0ac8a242489aa3d6fc67cb48739
Bruno Serra <brunoserra1965@gmail.com>
2020-06-28 23:10:20 -0300

    - agrego cosas en notebook por error de merge.

--- Notebooks/DNN - Image Classification/NNUrudateana.py
+++ Notebooks/DNN - Image Classification/NNUrudateana.py
@@ -192,7 +192,7 @@ def load_data():
     
     return train_set_x_orig, train_set_y_orig, test_set_x_orig, test_set_y_orig, classes
 
-def load_data_pillo():
+def load_data_real():
     train_dataset = h5py.File('datasets/cats_dogs_128.hdf5', "r")
     train_set_x_orig = np.array(train_dataset["train_img"][:]) # your train set features
     train_set_y_orig = np.array(train_dataset["train_labels"][:]) # your train set labels
